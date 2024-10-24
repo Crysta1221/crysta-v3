329531087855fb9fd37432b2f474842f0558361d
Crysta1221 <takuyakojim@outlook.jp>
2024-10-24 23:51:20 +0900

    :sparkles: add animation

--- src/components/navbar/theme-switch.tsx
+++ src/components/navbar/theme-switch.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
+import { motion, AnimatePresence } from "framer-motion";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
@@ -11,11 +12,29 @@ export function ModeToggle() {
   return (
     <Button
       variant='ghost'
-      className='rounded-full size-9 border-none'
+      className='rounded-full size-9 border md:border-none hover:bg-white hover:dark:bg-neutral-700'
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      <Sun className='h-[1.5rem] w-[1.3rem] dark:hidden' />
-      <Moon className='hidden h-5 w-5 dark:block' />
-      <span className='sr-only'>Toggle theme</span>
+      <AnimatePresence mode='wait' initial={false}>
+        <motion.span
+          initial={{
+            opacity: 0,
+            y: -15,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          exit={{
+            opacity: 0,
+            y: 15,
+          }}
+          transition={{
+            type: "keyframes",
+          }}
+          key={theme}>
+          {theme === "dark" ? <Moon size={24} /> : <Sun size={24} />}
+        </motion.span>
+      </AnimatePresence>
     </Button>
   );
 }
