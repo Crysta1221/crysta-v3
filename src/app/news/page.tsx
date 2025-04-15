072a0e938cc2d03fbc8a71c57e1fda415fa1fbfe
Crysta1221 <takuyakojim@outlook.jp>
2025-04-16 00:56:19 +0900

    :test_tube: スクロールエリアのテスト

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -6,6 +6,22 @@ export default function News() {
       <div className='container mx-auto px-8 py-24'>
         <PageHeading title='About' />
       </div>
+      <div className='mt-96 mb-96'></div>
+      <div className='py-96 '>
+        <p className='text-center text-xl font-bold'>
+          スクロールテスト用の余白
+        </p>
+      </div>
+      <div className='py-96'>
+        <p className='text-center text-xl font-bold'>
+          スクロールテスト用の余白
+        </p>
+      </div>
+      <div className='py-96'>
+        <p className='text-center text-xl font-bold'>
+          スクロールテスト用の余白
+        </p>
+      </div>
     </main>
   );
 }
