f5d73912aed0d025926930dda47d7ad2fc166258
Crysta1221 <takuyakojim@outlook.jp>
2025-04-22 18:13:24 +0900

    :art: レイアウト、位置の調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { HomeCarousel } from "@/components/carousel";
 
 export default function Home() {
   return (
-    <main className='relative mx-auto flex h-full ml-6 md:mx-8 lg:mx-0 lg:ml-6 flex-col justify-center my-18 sm:my-18 sm:pt-20 2xl:pt-14'>
-      <div className='mt-24 md:mt-16'>
+    <main className='relative mx-auto flex h-full flex-col justify-center my-18 sm:my-18 sm:pt-20 2xl:pt-14'>
+      <div className='mt-24 sm:mt-12 md:mt-16'>
         <HomeCarousel />
       </div>
     </main>
--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -3,7 +3,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { SwiperOptions } from "swiper/types";
@@ -46,9 +46,8 @@ export const HomeCarousel = () => {
   const swiperRef = useRef<any>(null);
   const breakpoints: SwiperOptions["breakpoints"] = {
     0: {
-      slidesPerView: 1.2,
-      spaceBetween: 5,
-      centeredSlides: false,
+      slidesPerView: 1,
+      centeredSlides: true,
     },
     640: {
       slidesPerView: 1,
@@ -72,46 +71,48 @@ export const HomeCarousel = () => {
   };
   return (
     <div className='flex flex-col'>
-      {" "}
-      <Swiper
-        className='w-full h-full'
-        slidesPerView={1.1}
-        spaceBetween={15}
-        centeredSlides={false}
-        breakpoints={breakpoints}
-        modules={[Pagination]}
-        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-        initialSlide={0}>
-        {" "}
-        {slide_data.map((slide, index) => (
-          <SwiperSlide key={index} className='px-2 py-2'>
-            <div className='relative w-full aspect-[3/2.5] sm:aspect-[16/7] md:aspect-[20/10] lg:aspect-[16/8] flex flex-col items-center justify-center rounded-2xl sm:rounded-3xl overflow-hidden'>
-              {" "}
-              <Image
-                src={slide.image}
-                alt={slide.title}
-                width={1000}
-                height={500}
-                className='object-cover w-full h-full'
-              />
-              <div className='absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-5'></div>{" "}
-              <div className='absolute bottom-0 mx-6 mb-4 left-0 right-0 p-4 flex justify-between items-end'>
-                <div className='text-white text-left'>
-                  <h2 className='hidden sm:block text-xl sm:text-2xl font-bold'>
-                    {slide.title}
-                  </h2>
-                  <p className='mt-1 text-white/80 line-clamp-2 sm:line-clamp-3'>
-                    {slide.description}
-                  </p>
+      <div className='mx-6 md:mx-8 lg:mx-0 lg:ml-6'>
+        <Swiper
+          className='w-full h-full'
+          slidesPerView={1.1}
+          spaceBetween={15}
+          centeredSlides={false}
+          breakpoints={breakpoints}
+          modules={[Pagination]}
+          onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          initialSlide={0}>
+          {slide_data.map((slide, index) => (
+            <SwiperSlide key={index} className='px-2 py-2'>
+              <p className='ml-2 sm:hidden font-bold text-lg mb-2 sm:mb-0'>
+                {slide.title}
+              </p>
+              <div className='relative w-full aspect-[3/2.5] sm:aspect-[16/7] md:aspect-[20/10] lg:aspect-[16/8] flex flex-col items-center justify-center rounded-2xl sm:rounded-3xl overflow-hidden'>
+                <Image
+                  src={slide.image}
+                  alt={slide.title}
+                  width={1000}
+                  height={500}
+                  className='object-cover w-full h-full'
+                />
+                <div className='absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-5'></div>{" "}
+                <div className='absolute bottom-0 mx-2 lg:mx-6 mb-4 left-0 right-0 p-2 lg:p-4 flex justify-between items-end'>
+                  <div className='text-white text-left'>
+                    <h2 className='hidden sm:block text-xl sm:text-2xl font-bold'>
+                      {slide.title}
+                    </h2>
+                    <p className='mt-1 text-white line-clamp-2 sm:line-clamp-3'>
+                      {slide.description}
+                    </p>
+                  </div>
+                  <Button className='rounded-full'>{slide.label}</Button>
                 </div>
-                <Button className='rounded-full'>{slide.label}</Button>
               </div>
-            </div>{" "}
-          </SwiperSlide>
-        ))}
-      </Swiper>{" "}
-      <div className='flex justify-center mt-4'>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className='flex justify-center items-center mt-4'>
         <div className='flex gap-2'>
           {slide_data.map((_, index) => (
             <div
@@ -120,7 +121,7 @@ export const HomeCarousel = () => {
               className={`h-2 w-2 rounded-full cursor-pointer ${
                 activeIndex === index
                   ? "bg-black dark:bg-white"
-                  : "bg-neutral-400 dark:bg-neutral-600"
+                  : "bg-neutral-300 dark:bg-neutral-700"
               }`}
             />
           ))}
